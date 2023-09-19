b9355e70a66b1b3c9ad62e7cee812546fb1cbd9f
Mukul Kumar <mukulninjas@gmail.com>
2023-09-19 16:39:23 +0530

    login implementation

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,21 +7,21 @@ router.post('/signup', async (req, res) => {
     try {
         const { name, email, password } = req.body;
         if (!(name && email && password)) {
-            res.status(400).json({ error: 'All fields are required' });
+            return res.status(400).json({ error: 'All fields are required' });
         }
 
         const oldUser = await usersModel.findOne({ email });
         if (oldUser) { 
-            res.status(400).json({ error: 'User already exists, Please Login !!' });
+           return res.status(400).json({ error: 'User already exists, Please Login !!' });
         }
 
         const newUser = new usersModel({ name, email, password });
 
         const saveUser = await newUser.save();
-        res.status(201).json({message: 'User saved successfully', user: saveUser});
+        return res.status(201).json({message: 'User saved successfully', user: saveUser});
     } catch (error) {
         console.log(error);
-        res.status(500).json({error: 'Error occured while creating user'});
+        return res.status(500).json({error: 'Error occured while creating user'});
     }
 });
 
@@ -35,13 +35,16 @@ router.post('/login', async (req, res) => {
         const user = await usersModel.findOne({ email: email, password: password });
         if (user) {
             const token = jwt.sign({ email: user.email, name: user.name, id: user.id }, "tokendecoder", { expiresIn: "24hrs" });
-            return res.status(200).json({message: 'Login Successful',token});
+            res.cookie("token", token);
+            return res.status(200).json({ message: 'Login Successful', token });
         } else {
-            res.status(400).json({ error: 'Invalid email and password.' });
+            return res.status(400).json({ error: 'Invalid email and password.' });
         }
     } catch (error) {
         console.log(error);
-        res.status(500).json({ error: 'Error occured while creating user' });
+        return res.status(500).json({
+            error: 'Error occured while logging user'
+        });
     }
 });
 // Add a member to a group
