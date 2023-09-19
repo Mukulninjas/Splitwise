a583fc2af0a7db305feb91dece58a87c4266fd2b
Mukul Kumar <mukulninjas@gmail.com>
2023-09-19 13:04:27 +0530

    Login and Signup implementation

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const usersModel = require('../models/usersModel');
+const jwt = require('jsonwebtoken');
 
 router.post('/signup', async (req, res) => {
     try {
         const { name, email, password } = req.body;
+        if (!(name && email && password)) {
+            res.status(400).json({ error: 'All fields are required' });
+        }
+
+        const oldUser = await usersModel.findOne({ email });
+        if (oldUser) { 
+            res.status(400).json({ error: 'User already exists, Please Login !!' });
+        }
+
         const newUser = new usersModel({ name, email, password });
 
         const saveUser = await newUser.save();
@@ -18,10 +28,17 @@ router.post('/signup', async (req, res) => {
 router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
-        const newUser = new usersModel({ name, email, password });
+        if (!(email && password)) {
+            res.status(400).json({ error: 'All fields are required' });
+        }
 
-        const saveUser = await newUser.save();
-        res.status(201).json({ message: 'User saved successfully', user: saveUser });
+        const user = await usersModel.findOne({ email: email, password: password });
+        if (user) {
+            const token = jwt.sign({ email: user.email, name: user.name, id: user.id }, "tokendecoder", { expiresIn: "24hrs" });
+            return res.status(200).json({message: 'Login Successful',token});
+        } else {
+            res.status(400).json({ error: 'Invalid email and password.' });
+        }
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: 'Error occured while creating user' });
